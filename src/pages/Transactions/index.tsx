03f2946c804a2d9cfd96b544262f6334c4ebc931
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2023-04-05 14:33:02 -0300

    :sparkles: add transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import * as S from './styles';
 import { Header } from '@components/Header';
 import { Summary } from '@components/Summary';
 
@@ -6,6 +7,30 @@ export const Transactions = () => {
     <div>
       <Header />
       <Summary />
+      <S.Container>
+        <S.Table>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <S.PriceHighlight variant="income">
+                  R$ 12.000,00
+                </S.PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambúrguer</td>
+              <td>
+                <S.PriceHighlight variant="outcome">-R$ 59,00</S.PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </S.Table>
+      </S.Container>
     </div>
   );
 };
---
+++ src/pages/Transactions/styles.ts
@@ -0,0 +1,41 @@
+import styled, { css } from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1120px;
+  margin: 4rem auto 0;
+  padding: 0 1.5rem;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  border-collapse: separate;
+  border-spacing: 0 0.5rem;
+
+  td {
+    padding: 1.25rem 2rem;
+    background: ${({ theme }) => theme['gray-700']};
+
+    &:first-child {
+      border-top-left-radius: 6px;
+      border-bottom-left-radius: 6px;
+    }
+
+    &:last-child {
+      border-top-right-radius: 6px;
+      border-bottom-right-radius: 6px;
+    }
+  }
+
+`;
+
+interface PriceColorProps {
+  variant: 'income' | 'outcome';
+}
+
+export const PriceHighlight = styled.span<PriceColorProps>`
+  color: ${({ theme, variant }) => css`
+    ${variant === 'income' ? theme['green-300'] : theme['red-300']}
+  `};
+
+`;
